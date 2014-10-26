4eb3cc1e50bd5f72d92cfbe4d4a1ea845bcf1af9
Milos Prchlik <happz@happz.cz>
2014-10-26 10:51:51 +0100

    "Hello, world!" modified to use proper calling convention, including save & restore registers

--- examples/hello-world.asm
+++ examples/hello-world.asm
@@ -1,34 +1,50 @@
 data message, "Hello, world!"
 
 main:
-  loada r0, &message
-  loada r1, 13
+  loada r1, &message
+  loada r2, 13
   call __fn_writeln_prolog
   hlt r0
 
+__outb_prolog:
+  # > r1: port
+  # > r2: byte
+  out r1, r2 b
+  ret
+
 __fn_writeln_prolog:
-  # > r0: string address
-  # > r1: string length
-  #   r2: port
+  # > r1: string address
+  # > r2: string length
   #   r3: current byte
+  #   r4: port
+  push r3
+  push r4
+  loada r4, 0x100
+__fn_writeln_loop:
+  load r3, r1 b
+  push r1
+  push r4
+  pop r1
   push r2
   push r3
-  loada r2, 0x100
-__fn_writeln_loop:
-  # string
-  load r0, r3 b
-  out r3, r2 b
-  inc r0
-  dec r1
-  jnz __fn_writeln_loop
-  # \n
-  loada r3, 0xA b
-  out r3, r2 b
-  # \r
-  loada r3, 0xD b
-  out r3, r2 b
-  pop r3
   pop r2
+  call __outb_prolog
+  pop r2
+  pop r1
+  inc r1
+  dec r2
+  jnz __fn_writeln_loop
+  push r1
+  push r4
+  pop r1
+  # \n
+  loada r2, 0xA b
+  call __outb_prolog
+  loada r2, 0xD b
+  call __outb_prolog
+  pop r1
   loada r0, 0
+  pop r4
+  pop r3
   ret
 
