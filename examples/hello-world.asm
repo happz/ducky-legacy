62bda781662e75df8d6a4ac2c8d44a9091e76b5f
Milos Prchlik <happz@happz.cz>
2014-10-26 17:44:41 +0100

    Use mov instructions in example

--- examples/hello-world.asm
+++ examples/hello-world.asm
@@ -23,11 +23,9 @@ __fn_writeln_prolog:
 __fn_writeln_loop:
   load r3, r1 b
   push r1
-  push r4
-  pop r1
+  mov r1, r4
   push r2
-  push r3
-  pop r2
+  mov r2, r3
   call __outb_prolog
   pop r2
   pop r1
@@ -35,8 +33,7 @@ __fn_writeln_loop:
   dec r2
   jnz __fn_writeln_loop
   push r1
-  push r4
-  pop r1
+  mov r1, r4
   # \n
   loada r2, 0xA b
   call __outb_prolog
