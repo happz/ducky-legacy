76a29c46dc38660ce08cde992e5e1fc8df051b04
Milos Prchlik <happz@happz.cz>
2015-05-02 20:46:05 +0200

    Docs for reactor.py

--- src/reactor.py
+++ src/reactor.py
@@ -1,13 +1,43 @@
+"""
+This module provides simple reactor core that runs each of registered tasks at
+least once during one iteration of its internal loop.
+
+Reactor is a singleton, there should be no need to create Reactor instance.
+
+There are two different kinds of objects that reactor manages:
+
+- task - it's called periodicaly, at least once in each reactor loop iteration
+- event - asynchronous events are queued and executed before running any tasks.
+  If there are no runnable tasks, reactor loop waits for incomming events.
+"""
+
 import Queue
 
 class ReactorTask(object):
+  """
+  Base class for all reactor tasks.
+  """
+
   def runnable():
+    """
+    Returns ``True`` if task can be run.
+    """
+
     return False
 
   def run():
+    """
+    This method is called by reactor to perform task's actions.
+    """
+
     pass
 
 class CallInReactorTask(ReactorTask):
+  """
+  This task request running particular function during the reactor loop. Useful
+  for planning future work, and for running tasks in reactor's thread.
+  """
+
   def __init__(self, fn, *args, **kwargs):
     self.fn = fn
     self.args = args
@@ -20,6 +50,18 @@ class CallInReactorTask(ReactorTask):
     self.fn(*self.args, **self.kwargs)
 
 class Reactor(object):
+  """
+  Main reactor class. Never to be instantiated by application code, always
+  import ``reactor`` from this module:
+
+  >>> from reactor import reactor
+
+  Or:
+
+  >>> from reactor import Reactor
+  >>> reactor = Reactor.reactor()
+  """
+
   _reactor = None
 
   def __init__(self):
@@ -28,24 +70,52 @@ class Reactor(object):
 
   @staticmethod
   def reactor():
+    """
+    Return reactor. Reactor is a singleton, if there is an existing reactor
+    object, it is returned, otherwise a new one is created.
+    """
+
     if Reactor._reactor is None:
       Reactor._reactor = Reactor()
 
     return Reactor._reactor
 
   def add_task(self, task):
+    """
+    Register task with reactor's main loop.
+    """
+
     self.tasks.append(task)
 
   def remove_task(self, task):
+    """
+    Unregister task, it will never be run again.
+    """
+
     self.tasks.remove(task)
 
   def add_event(self, event):
+    """
+    Enqueue asynchronous event.
+    """
+
     self.events.put(event)
 
   def add_call(self, fn, *args, **kwargs):
+    """
+    Enqueue function call. Function will be called in reactor loop.
+    """
+
     self.add_event(CallInReactorTask(fn, *args, **kwargs))
 
   def run(self):
+    """
+    Starts reactor loop. Enters endless loop, calling runnable tasks and events,
+    and - in case there are no runnable tasks - waits for new events.
+
+    When there are no tasks managed by reactor, loop quits.
+    """
+
     while True:
       if not self.tasks:
         break
