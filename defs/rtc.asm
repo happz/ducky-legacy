9e8719f0ea62e6df620ed05bc8513fe99fb7ee48
Milos Prchlik <happz@happz.cz>
2015-12-01 16:31:56 +0100

    RTC assembly constants

---
+++ defs/rtc.asm
@@ -0,0 +1,14 @@
+;
+; RTC device defines
+;
+
+
+.def RTC_DEFAULT_FREQ:  100
+
+.def RTC_PORT_FREQ:     0x300
+.def RTC_PORT_SECOND:   0x301
+.def RTC_PORT_MINUTE:   0x302
+.def RTC_PORT_HOUR:     0x303
+.def RTC_PORT_DAY:      0x304
+.def RTC_PORT_MONTH:    0x305
+.def RTC_PORT_YEAR:     0x306
