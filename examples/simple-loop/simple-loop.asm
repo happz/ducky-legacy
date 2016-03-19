44786212d7288b75c066d5390431333a778c17e0
Milos Prchlik <happz@happz.cz>
2016-03-19 09:59:28 +0100

    Simple loopy benchmark example

---
+++ examples/simple-loop/simple-loop.asm
@@ -0,0 +1,36 @@
+;
+; Simple loopy benchmark. Not very sophisticated but simple and quite reloable.
+;
+
+.def L1_ITERS: 1000
+.def L2_ITERS: 1000
+
+  .data
+
+  .type l1_iters, int
+  .int $L1_ITERS
+
+  .type l2_iters, int
+  .int $L2_ITERS
+
+  .text
+
+  li r2, 0
+  la r0, &l1_iters
+  lw r0, r0
+  la r3, &l2_iters
+  lw r3, r3
+.l1_loop:
+  bz &.l1_quit
+  mov r1, r3
+.l2_loop:
+  bz &.l2_quit
+  add r2, 8
+  sub r2, 8
+  dec r1
+  j &.l2_loop
+.l2_quit:
+  dec r0
+  j &.l1_loop
+.l1_quit:
+  hlt 0x00
