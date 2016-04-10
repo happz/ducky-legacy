39d5010acdd460ba36325fc39c7f77a5766185f5
Milos Prchlik <happz@happz.cz>
2016-04-10 19:02:43 +0200

    Hypothesis profiles are in config file now
    And raising CI examples to 4k, CAS test failed. Maybe it will be
    necessary to rewrite the test.

--- tests/instructions.py
+++ tests/instructions.py
@@ -11,22 +11,38 @@ from ducky.cpu.registers import REGISTER_NAMES
 from ducky.errors import UnalignedJumpTargetError, AccessViolationError, InvalidResourceError
 from ducky.mm import u32_t, i32_t
 
+from testconfig import config
+
 from hypothesis import given, example, assume, settings
 from hypothesis.strategies import integers, lists, booleans, composite
 
 PYPY = hasattr(sys, 'pypy_version_info')
 
+LOGGER = logging.getLogger()
+
 #
 # Hypothesis setup
 #
-settings.register_profile('Sanity', settings(max_examples = 50))
-settings.register_profile('Default', settings(max_examples = 200))
-settings.register_profile('CI', settings(max_examples = 2000))
+DEFAULT_EXAMPLES = 200
 
-settings.load_profile(os.environ.get('HYPOTHESIS_PROFILE', 'Default'))
+if 'HYPOTHESIS_PROFILE' in os.environ:
+  profile = os.environ['HYPOTHESIS_PROFILE'].lower()
+
+  if profile not in config['hypothesis']:
+    LOGGER.warning('Unknown hypothesis profile "%s"', profile)
+    profile = settings(max_examples = DEFAULT_EXAMPLES)
+
+  else:
+    profile = settings(max_examples = int(config['hypothesis'][profile]))
+
+  settings.register_profile('ducky-profile', profile)
+
+else:
+  settings.register_profile('ducky-profile', settings(max_examples = DEFAULT_EXAMPLES))
+
+settings.load_profile('ducky-profile')
 
 CORE = None
-LOGGER = logging.getLogger()
 BUFFER = None
 
 def setup():
