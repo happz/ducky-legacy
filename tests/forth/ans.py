5f43a889bf0606f0c6574efe3678526f2fe748d4
Milos Prchlik <happz@happz.cz>
2016-04-27 21:02:28 +0200

    Remove ANS error report, some weird diffs when running with pypy

--- tests/forth/ans.py
+++ tests/forth/ans.py
@@ -3,8 +3,6 @@ from . import run_forth_vm, logs_dir, tests_dir
 def test_ans():
   options = [
     ('--add-option=device-3:streams_in=%s' % tests_dir('forth', 'ans-testsuite', 'src', f)) for f in ['prelimtest.fth', 'tester.fr', 'core.fr', 'coreplustest.fth', 'utilities.fth', 'errorreport.fth']
-  ] + [
-    '--add-option=device-3:streams_in=%s' % tests_dir('forth', 'ans-report.f')
   ]
 
   return run_forth_vm(out = logs_dir('forth-ans.out'), machine = logs_dir('forth-ans.machine'), options = options, coverage_name = 'forth-ans', diff_expected = ('forth', 'ans.expected'))
