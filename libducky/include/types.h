4ea69c5688682c8a1320ac747a84d715920100e7
Milos Prchlik <happz@happz.cz>
2016-11-10 22:06:39 +0100

    [libducky] Add i64_t and u64_t integer types

--- libducky/include/types.h
+++ libducky/include/types.h
@@ -9,6 +9,8 @@ typedef signed short i16_t;
 typedef unsigned short u16_t;
 typedef signed int i32_t;
 typedef unsigned int u32_t;
+typedef signed long long i64_t;
+typedef unsigned long long u64_t;
 
 typedef u32_t uptr_t;
 typedef i32_t iptr_t;
